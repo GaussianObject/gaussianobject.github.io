1dfdde9ecbbefc9565ce93dec9298a04f35f11e8
Uranus <whlisikuang@outlook.com>
2024-02-06 01:41:24 +0800

    improve preload

--- static/js/preload-videos.js
+++ static/js/preload-videos.js
@@ -26,9 +26,10 @@ async function preloadVideo(url) {
 async function preloadVideos() {
   methods = ["3DGS", "DVGO", "DietNeRF", "RegNeRF", "FreeNeRF", "SparseNeRF", "ZeroRF", "FSGS"];
   scenes = ["omni3d_dinosaur_006", "mip360_bonsai", "mip360_garden", "mip360_kitchen", "omni3d_broccoli_003", "omni3d_suitcase_006", "omni3d_hamburger_012", "omni3d_pineapple_013", "omni3d_vase_012", "oppo_obj_27_pumpkin2", "oppo_obj_29_fabric_toy"];
-  for (var i = 0; i < methods.length; i++) {
-    for (var j = 0; j < scenes.length; j++) {
+  for (var j = 0; j < scenes.length; j++) {
+    for (var i = 0; i < methods.length; i++) {
       await preloadVideo("static/videos/comparison/" + scenes[j] + "_4_" + methods[i] + ".mp4");
+      console.log("Preloaded: " + scenes[j] + "_4_" + methods[i] + ".mp4");
     }
   }
 }
